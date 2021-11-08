ab54b76c0e08e6e8043e757a3610af20f663a754
Manjil Tamang <lamamanjil@gmail.com>
2021-11-08 18:45:30 +0545

    Complete yatri store strapi api v1

--- strapi-api/config/server.js
+++ strapi-api/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 3006),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '15db3c83c0ffe6e6fa047b689da52174'),
+      secret: env("ADMIN_JWT_SECRET", "15db3c83c0ffe6e6fa047b689da52174"),
     },
   },
 });
