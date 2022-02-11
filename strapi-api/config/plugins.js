92614e23b7692b9e518efd80ccceb4f246d70312
Manjil Tamang <lamamanjil@gmail.com>
2022-02-11 21:26:24 +0545

    Add sendgrid email plugin

--- strapi-api/config/plugins.js
+++ strapi-api/config/plugins.js
@@ -10,4 +10,13 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    provider: "sendgrid",
+    providerOptions: {
+      apiKey: env("SENDGRID_API_KEY"),
+    },
+    settings: {
+      defaultFrom: "no-reply@yatrimotorcycles.com",
+    },
+  },
 });
