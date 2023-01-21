98e6451becb516907bb71b98e5aa25babde3e3fa
Manjil Tamang <lamamanjil@gmail.com>
2023-01-22 01:30:09 +0545

    Create product order module

--- strapi-api/api/order/models/order.js
+++ strapi-api/api/order/models/order.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
+const { v4: uuid } = require("uuid");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      data.published_at = new Date();
+      if (!data.uuid) {
+        data.uuid = uuid();
+      }
+    },
+  },
+};
